d91fffcdc09499ebd89d23607382e68b629a87bc
Isaac <isaacmkelly0@gmail.com>
2023-08-17 16:37:55 -0500

    this one's gonna be hard

---
+++ ch7/strip.py
@@ -0,0 +1,9 @@
+import re
+def strip(string, thing):
+    if thing:
+        notThisRegex = re.compile(r'') #TODO
+    else:
+        notThisRegex = re.compile(r'^\s+')
+    pole = notThisRegex.search(string)
+    print(pole)
+strip("    poo    ", False)
